916ca18946108f44e3f8b023bb2cb124406ce1a2
zerocordeiro <juseh.r@gmail.com>
2023-10-11 11:01:09 +0100

    App.jsx updated with import React...

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-// import { useState } from 'react'
-// import './App.css'
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
 
 import { Footer } from './Footer'
 import { Header} from './Header'
